fcbe52d71619904c78aa16a344cc0b08759b215e
fisamodo <fisamodo@vsite.hr>
2020-04-14 15:36:47 -0700

    go

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header"><h3>Your Scouted Player Register</h3></div>
